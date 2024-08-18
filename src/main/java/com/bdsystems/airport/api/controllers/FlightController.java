a0da4d55d488e3d8389f2fb66c2382071b9948b8
Benicius <beniciusclaro@hotmail.com>
2024-08-18 21:56:27 +0100

    remove volumes from docker image

--- src/main/java/com/bdsystems/airport/api/controllers/FlightController.java
+++ src/main/java/com/bdsystems/airport/api/controllers/FlightController.java
@@ -1,10 +1,7 @@
 package com.bdsystems.airport.api.controllers;
 
-import com.bdsystems.airport.api.domains.Flight;
 import com.bdsystems.airport.api.domains.FlightVo;
 import com.bdsystems.airport.api.services.flights.FlightService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +16,6 @@ public class FlightController {
 	public FlightController(FlightService flightService) {
 		this.flightService = flightService;
 	}
-
-	/*@PostMapping
-	public ResponseEntity<Flight> createFlight(@RequestBody final FlightVo flightVo){
-
-		return new ResponseEntity<>(flightService.save(flightVo), HttpStatus.CREATED);
-	}*/
 
 	@PostMapping
 	public String createProcess(@RequestBody final FlightVo flightVo){
